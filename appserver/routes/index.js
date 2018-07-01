e4f540841fdee89a87c3c89b6a3edaeaedcb04ae
a27kash <a27kash@gmail.com>
2018-07-01 15:57:37 +0530

    sample mysql query

--- appserver/routes/index.js
+++ appserver/routes/index.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../db');
 
 /* GET home page. */
 router.post('/', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  db.query('SELECT * FROM Persons', function (error, results, fields) {
+    if (error) throw error;
+    console.log(results);
+  });
   res.status(200).send(req.body);
 
 });
