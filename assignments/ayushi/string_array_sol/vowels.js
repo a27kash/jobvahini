0891d2c540c26319aa407c7263972c969ce29778
Ayushi Kulshreshtha <ayushikulshreshtha25@gmail.com>
2018-07-10 20:23:47 +0530

    assignment

---
+++ assignments/ayushi/string_array_sol/vowels.js
@@ -0,0 +1,11 @@
+var c = "It is alright";
+var s="";
+for(i=0;i<c.length;i=i+1)
+{
+    if(c.charAt(i)=='a'||c.charAt(i)=='e'||c.charAt(i)=='i'||c.charAt(i)=='o'||c.charAt(i)=='u')
+    {
+        c.charAt(i)=='-';
+    }
+s=s+c.charAt(i);
+}
+console.log(s)
