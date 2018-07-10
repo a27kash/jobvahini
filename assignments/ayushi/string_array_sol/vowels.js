8bd25bbd998c04314d18e3cf706a7690ec7bc3bb
Ayushi Kulshreshtha <ayushikulshreshtha25@gmail.com>
2018-07-10 20:42:10 +0530

    assignment

--- assignments/ayushi/string_array_sol/vowels.js
+++ assignments/ayushi/string_array_sol/vowels.js
@@ -4,7 +4,7 @@ for(i=0;i<c.length;i=i+1)
 {
     if(c.charAt(i)=='a'||c.charAt(i)=='e'||c.charAt(i)=='i'||c.charAt(i)=='o'||c.charAt(i)=='u')
     {
-        c.charAt(i)=='-';
+        c.charAt(i)='-';
     }
 s=s+c.charAt(i);
 }
